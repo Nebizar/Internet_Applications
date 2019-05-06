34d06b624530f126e4d21ff7207b7455f8093eda
Krzysztof <krzysztof211@gmail.com>
2019-05-06 11:14:13 +0200

    basic mcv routes

--- ASP.NET MVC/MVCTutorial/App_Start/RouteConfig.cs
+++ ASP.NET MVC/MVCTutorial/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace MVCTutorial
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Square",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Songs", action = "Square", id = 23 }
+            );
         }
     }
 }
---
+++ ASP.NET MVC/MVCTutorial/Controllers/SongsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class SongsController : Controller
+    {
+        // GET: Songs
+        public ActionResult Index()
+        {
+            return Content("Hello World!");
+        }
+
+        // GET: Songs/Square/<id>
+        public ActionResult Square(int id)
+        {
+            return Content((id*id).ToString());
+        }
+    }
+}
